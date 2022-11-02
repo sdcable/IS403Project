c6e1fba32bab3dff71faf41f1231bb9c288ccc76
meganlucille <megan@mightyjohnsons.com>
2022-11-02 11:29:50 -0600

    hi

--- ActivityTracker/ActivityTracker/Activities/views.py
+++ ActivityTracker/ActivityTracker/Activities/views.py
@@ -10,7 +10,7 @@ def Darby(request):
     return HttpResponse('Hello Darby! Welcome to your page!') #This will be Darby's Page
 
 def Megan(request):
-    return HttpResponse('Hello Megan Johnson!')
+    return HttpResponse('Hello Megan Johnson! wahoo') #This will be megans change
 
 def Carter(request):
     return HttpResponse('Hello Carter! test') #This will be Carter's page :))))))
